cc37937e6891c3f5c2a361f2e309b67c559efe0e
Young Chan Han <hanyc1205@gmail.com>
2025-04-08 12:43:29 -0400

    Header and Footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Page() {
-  return (
-    <>
-      <Button>Button</Button>
-      <div>awefaa</div>
-    </>
-  );
+  return <div>Prostore</div>;
 }
