65ede58912c28eeb1d1a83f8bdc78c7bd33941f6
Young Chan Han <hanyc1205@gmail.com>
2025-04-18 22:42:50 -0400

    removed ghost on sign in button

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -28,7 +28,7 @@ const Header = () => {
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign In
             </Link>
