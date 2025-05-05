200d790c832931869fc6182a12fea3f39e5bb96a
Young Chan Han <hanyc1205@gmail.com>
2025-05-05 11:15:25 -0400

    test for velcel

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,3 +1,4 @@
+import { Product } from "@/types";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
